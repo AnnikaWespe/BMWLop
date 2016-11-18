b06806fd463d06ed98ebc3dc6bd384e81dddcb58
AnnikaWespe <annika.westphaeling@cgi.com>
2016-11-18 18:34:31 +0100

    Add files via upload

--- LOP_Puma_template.js
+++ LOP_Puma_template.js
@@ -7,10 +7,16 @@ var columnCopyHistory = "wq1z";
 var urlInfoSign = '/sites/VSC/SiteCollectionImages/Informationsign.png';
 var urlCopySign = "/sites/VSC/SiteCollectionImages/Copy.png";
 var bootstrapTooltip = '<a href="#" data-html="true" data-toggle="tooltip" style ="white-space: pre-wrap;min-width: 100px;" title="';
+var currentListName;
 
 
 $(document).ready(function() {
-    $SP().list("LOP / Puma Template").info(function(fields) {
+    //get name of current list
+    var url = window.location.href;
+    var relativeUrl = url.replace("https://vts5.bmwgroup.net", "");
+    currentListName = $("#pageTitle").find("a[href='" + relativeUrl + "']").text();
+
+    $SP().list(currentListName).info(function(fields) {
         var nameDisplaynameMap = {};
         var displaynameNameMap = {};
         var nameColumnNumberMap = {};
@@ -23,7 +29,7 @@ $(document).ready(function() {
             nameColumnNumberMap[key] = i;
             console.log(key + "    " + value);
         };
-        $SP().list("LOP / Puma Template").get(function(data) {
+        $SP().list(currentListName).get(function(data) {
             var arrayOfHiddenColumns = [columnGeaendertAm, columnGeaendertVon, columnErfasstAm, columnErstelltVon, columnCopyHistory];
             var numberOfHiddenColumns = arrayOfHiddenColumns.length;
             var numberOfRows = data.length;
@@ -52,11 +58,22 @@ $(document).ready(function() {
         })
     });
     $SP().lists(function(list) {
-        var availableListsSelect = "<select class='availableListsSelect'>";
+        var availableListsSelect = "<select id='availableListsSelect'>";
         var numberOfAvailableLists = list.length;
         var $copySigns = $("img[src='" + urlCopySign + "']");
+        var typeOfList;
+        //get the select to display only suitable lists for copying
+        if (currentListName.indexOf("LOP") !== -1) {
+            typeOfList = "LOP";
+        } else if (currentListName.indexOf("PUMA") !== -1) {
+            typeOfList = "PUMA"
+        } else {
+            typeOfList = "MeP"
+        };
         for (var i = 0; i < numberOfAvailableLists; i++) {
-            availableListsSelect += "<option>" + list[i]['Name'] + "</option>";
+            if (list[i]['Name'].indexOf("LOP") !== -1) {
+                availableListsSelect += "<option>" + list[i]['Name'] + "</option>";
+            }
         };
         availableListsSelect += "</select>";
         $copySigns.each(function(index, value) {
@@ -66,8 +83,7 @@ $(document).ready(function() {
                 '</p><a href="#" rel="modal:close"><button class = "btn btn-default">Abbrechen</button></a>' +
                 '<button class="btn btn-default" id="Kopiere' + index + '"" style="float: right">Kopieren</button>');
             $("#Kopiere" + index).click(function() {
-                console.log("Kopiere Index " + index + " in Liste " + $(".availableListsSelect option:selected").text());
-
+                console.log("Kopiere Index " + index + " in Liste " + $("#availableListsSelect option:selected").text());
             });
         });
     });
