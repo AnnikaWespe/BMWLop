424bcdc927c8bbbb0fd16ed0535ae0c4ec38a8d0
AnnikaWespe <annika.westphaeling@cgi.com>
2016-11-18 15:17:18 +0100

    Add files via upload

--- LOP_Puma_template.aspx
+++ LOP_Puma_template.aspx
@@ -7,10 +7,12 @@
 		<script src="/sites/VSC/LOP%20%20Puma%20Scripts/bootstrap.min.js" type="text/javascript"></script>
 		<script src="/sites/VSC/LOP%20%20Puma%20Scripts/bootstrap.min.js"></script>
 		<script src="/sites/VSC/LOP%20%20Puma%20Scripts/sharepointplus-3.13.min.js" type="text/javascript"></script>
+		<script src="/sites/VSC/LOP%20%20Puma%20Scripts/jquery.modal.js" type="text/javascript" charset="utf-8"></script>
 
 		<link href="/sites/VSC/LOP%20%20Puma%20Scripts/bootstrap.min.css" rel="stylesheet" media="screen">
+		<link href="/sites/VSC/LOP%20%20Puma%20Scripts/jquery.modal.css" rel="stylesheet" media="screen">
 		<link href="/sites/VSC/LOP%20%20Puma%20Scripts/scripts/LOP_Puma_template.css" rel="stylesheet" type="text/css"/>
 	</head>
-	
+	<div class="appendModales"></div>
 		<script src="/sites/VSC/LOP%20%20Puma%20Scripts/scripts/LOP_Puma_template.js" type="text/javascript"></script>
 </html>
--- LOP_Puma_template.js
+++ LOP_Puma_template.js
@@ -1,11 +1,12 @@
 //Strings
-var columnGeaendertVon = "g9fj";
-var columnGeaendertAm = "q5hp";
-var columnEmail = "Email";
-var columnLegende = "f1za";
-var columnErfasstAm = "d3o3";
+var columnGeaendertVon = "Editor";
+var columnGeaendertAm = "Last_x0020_Modified";
+var columnErfasstAm = "Created_x0020_Date";
 var columnErstelltVon = "Author";
+var columnCopyHistory = "wq1z";
 var urlInfoSign = '/sites/VSC/SiteCollectionImages/Informationsign.png';
+var urlCopySign = "/sites/VSC/SiteCollectionImages/Copy.png";
+var bootstrapTooltip = '<a href="#" data-html="true" data-toggle="tooltip" style ="white-space: pre-wrap;min-width: 100px;" title="';
 
 
 $(document).ready(function() {
@@ -23,34 +24,52 @@ $(document).ready(function() {
             console.log(key + "    " + value);
         };
         $SP().list("LOP / Puma Template").get(function(data) {
-            var arrayOfHiddenColumns = [columnGeaendertVon, columnGeaendertAm, columnEmail, columnLegende, columnErfasstAm, columnErstelltVon];
+            var arrayOfHiddenColumns = [columnGeaendertAm, columnGeaendertVon, columnErfasstAm, columnErstelltVon, columnCopyHistory];
             var numberOfHiddenColumns = arrayOfHiddenColumns.length;
             var numberOfRows = data.length;
             for (var j = 0; j < numberOfRows; j++) {
                 infoText[j] = "";
-                for (var i = 1; i < numberOfHiddenColumns; i++) {
+                for (var i = 0; i < numberOfHiddenColumns; i++) {
                     var currentColumnName = arrayOfHiddenColumns[i];
                     var currentColumnNumber = nameColumnNumberMap[currentColumnName];
                     var currentColumnDisplayName = nameDisplaynameMap[currentColumnName];
                     var currentEntry = data[j].getAttribute(currentColumnName);
-                    if (currentEntry == "null") {
+                    if (currentEntry == null) {
                         currentEntry = " ";
                     };
-                    if (currentColumnName == columnErstelltVon) {
+                    if (currentColumnName == columnGeaendertVon || currentColumnName == columnErstelltVon || currentColumnName == columnErfasstAm || currentColumnName == columnGeaendertAm) {
                         var helperArray = currentEntry.split("#");
                         currentEntry = helperArray[1];
                     }
-                    infoText[j] += currentColumnDisplayName + ": " + currentEntry + "\n";
-                }
+                    infoText[j] += currentColumnDisplayName + ": " + currentEntry + "&lt;br /&gt;";
+                };
             };
             var $infoSigns = $("img[src='" + urlInfoSign + "']");
             $infoSigns.each(function(index, value) {
-                $(this).wrap('<a href="#" data-toggle="tooltip" title="' + infoText[index] + '"></a>');
-                $(this).tooltip();
-                $(this).click(function() {
-                    alert("you seriously clicked me")
-                })
+                $(this).wrap(bootstrapTooltip + infoText[index] + '"></a>');
+                $(this).closest("a").tooltip();
             })
         })
-    })
+    });
+    $SP().lists(function(list) {
+        var availableListsSelect = "<select class='availableListsSelect'>";
+        var numberOfAvailableLists = list.length;
+        var $copySigns = $("img[src='" + urlCopySign + "']");
+        for (var i = 0; i < numberOfAvailableLists; i++) {
+            availableListsSelect += "<option>" + list[i]['Name'] + "</option>";
+        };
+        availableListsSelect += "</select>";
+        $copySigns.each(function(index, value) {
+            $(this).wrap('<a href="#modular' + index + '" rel="modal:open"></a>');
+            $(".appendModales").append('<div id="modular' + index + '" style="display:none;">' +
+                '<p>In welche Liste möchten Sie die Daten kopieren?</p><p>' + availableListsSelect +
+                '</p><a href="#" rel="modal:close"><button class = "btn btn-default">Abbrechen</button></a>' +
+                '<button class="btn btn-default" id="Kopiere' + index + '"" style="float: right">Kopieren</button>');
+            $("#Kopiere" + index).click(function() {
+                console.log("Kopiere Index " + index + " in Liste " + $(".availableListsSelect option:selected").text());
+
+            });
+        });
+    });
+
 })
