efff25e548a437a471b4a78df8f839cf54bb6996
AnnikaWespe <annika.westphaeling@cgi.com>
2016-11-21 19:23:39 +0100

    Add files via upload

--- LOP_Puma_template.aspx
+++ LOP_Puma_template.aspx
@@ -8,11 +8,20 @@
 		<script src="/sites/VSC/LOP%20%20Puma%20Scripts/bootstrap.min.js"></script>
 		<script src="/sites/VSC/LOP%20%20Puma%20Scripts/sharepointplus-3.13.min.js" type="text/javascript"></script>
 		<script src="/sites/VSC/LOP%20%20Puma%20Scripts/jquery.modal.js" type="text/javascript" charset="utf-8"></script>
-
 		<link href="/sites/VSC/LOP%20%20Puma%20Scripts/bootstrap.min.css" rel="stylesheet" media="screen">
 		<link href="/sites/VSC/LOP%20%20Puma%20Scripts/jquery.modal.css" rel="stylesheet" media="screen">
 		<link href="/sites/VSC/LOP%20%20Puma%20Scripts/scripts/LOP_Puma_template.css" rel="stylesheet" type="text/css"/>
 	</head>
-	<div class="appendModales">Here are your Modales</div>
-		<script src="/sites/VSC/LOP%20%20Puma%20Scripts/scripts/LOP_Puma_template.js" type="text/javascript"></script>
+	<div class="appendModales">
+		<div id="modular" style="display:none;">
+			<p>In welche Liste wollen Sie die Daten kopieren?</p>
+			<div id="appendSelectHere">
+			</div>
+			<a href="#" rel="modal:close">
+				<button class = "btn btn-default">Abbrechen</button>
+			</a>
+			<button class="btn btn-default" id="Kopieren" style="float: right">Kopieren</button>
+		</div>
+	</div>
+	<script src="/sites/VSC/LOP%20%20Puma%20Scripts/scripts/LOP_Puma_template.js" type="text/javascript"></script>
 </html>
--- LOP_Puma_template.js
+++ LOP_Puma_template.js
@@ -11,85 +11,86 @@ var currentListName;
 
 
 $(document).ready(function() {
-    //get name of current list
-    var url = window.location.href;
-    var relativeUrl = url.replace("https://vts5.bmwgroup.net", "");
-    var $copySigns = $("img[src='" + urlCopySign + "']");
-    currentListName = $("#pageTitle").find("a[href='" + relativeUrl + "']").text();
+	//get name of current list
+	var url = window.location.href;
+	var relativeUrl = url.replace("https://vts5.bmwgroup.net", "");
+	var $copySigns = $("img[src='" + urlCopySign + "']");
+	currentListName = $("#pageTitle").find("a[href='" + relativeUrl + "']").text();
 
-    $SP().list(currentListName).info(function(fields) {
-        var nameDisplaynameMap = {};
-        var displaynameNameMap = {};
-        var nameColumnNumberMap = {};
-        var infoText = [];
-        for (var i = 0; i < fields.length; i++) {
-            var key = fields[i]["Name"];
-            var value = fields[i]["DisplayName"];
-            nameDisplaynameMap[key] = value;
-            displaynameNameMap[value] = key;
-            nameColumnNumberMap[key] = i;
-            console.log(key + "    " + value);
-        };
-        $SP().list(currentListName).get(function(data) {
-            var arrayOfHiddenColumns = [columnGeaendertAm, columnGeaendertVon, columnErfasstAm, columnErstelltVon, columnCopyHistory];
-            var numberOfHiddenColumns = arrayOfHiddenColumns.length;
-            var numberOfRows = data.length;
-            for (var j = 0; j < numberOfRows; j++) {
-                infoText[j] = "";
-                for (var i = 0; i < numberOfHiddenColumns; i++) {
-                    var currentColumnName = arrayOfHiddenColumns[i];
-                    var currentColumnNumber = nameColumnNumberMap[currentColumnName];
-                    var currentColumnDisplayName = nameDisplaynameMap[currentColumnName];
-                    var currentEntry = data[j].getAttribute(currentColumnName);
-                    if (currentEntry == null) {
-                        currentEntry = " ";
-                    };
-                    if (currentColumnName == columnGeaendertVon || currentColumnName == columnErstelltVon || currentColumnName == columnErfasstAm || currentColumnName == columnGeaendertAm) {
-                        var helperArray = currentEntry.split("#");
-                        currentEntry = helperArray[1];
-                    }
-                    infoText[j] += currentColumnDisplayName + ": " + currentEntry + "&lt;br /&gt;";
-                };
-            };
-            var $infoSigns = $("img[src='" + urlInfoSign + "']");
-            $infoSigns.each(function(index, value) {
-                $(this).wrap(bootstrapTooltip + infoText[index] + '"></a>');
-                $(this).closest("a").tooltip();
-            });
-            $SP().lists(function(list) {
-                var availableListsSelect = document.createElement("select");
-                // availableListsSelect.id = "availableListsSelect";
-                var numberOfAvailableLists = list.length;
-                var typeOfList;
-                //get the select to display only suitable lists for copying
-                if (currentListName.indexOf("LOP") !== -1) {
-                    typeOfList = "LOP";
-                } else if (currentListName.indexOf("PUMA") !== -1) {
-                    typeOfList = "PUMA"
-                } else {
-                    typeOfList = "MeP"
-                };
-                for (var i = 0; i < numberOfAvailableLists; i++) {
-                    var currentListChecked = list[i]['Name'];
-                    if (currentListChecked.indexOf("LOP") !== -1) {
-                        var option = document.createElement("option");
-                        option.value = currentListChecked;
-                        option.text = currentListChecked;
-                        availableListsSelect.appendChild(option);
-                    }
-                };
-                $copySigns.each(function(index, value) {
-                    $(this).wrap('<a href="#modular' + index + '" rel="modal:open"></a>');
-                    $(".appendModales").append('<div id="modular' + index + '" style="display:none;">' +
-                        '<p>In welche Liste möchten Sie die Daten kopieren?</p><div id="appendSelectHere"></div><a href="#" rel="modal:close"><button class = "btn btn-default">Abbrechen</button></a>' +
-                        '<button class="btn btn-default" id="Kopiere' + index + '"" style="float: right">Kopieren</button>');
-                    document.getElementById("appendSelectHere").appendChild(availableListsSelect);
-                    $("#Kopiere" + index).click(function() {
-                        console.log("Kopiere Index " + index + " in Liste " + $("#availableListsSelect").find(":selected").text());
-                        alert(data[index].getAttribute("ID"));
-                    });
-                });
-            });
-        })
-    });
+	$SP().list(currentListName).info(function(fields) {
+		var nameDisplaynameMap = {};
+		var displaynameNameMap = {};
+		var nameColumnNumberMap = {};
+		var infoText = [];
+		for (var i = 0; i < fields.length; i++) {
+			var key = fields[i]["Name"];
+			var value = fields[i]["DisplayName"];
+			nameDisplaynameMap[key] = value;
+			displaynameNameMap[value] = key;
+			nameColumnNumberMap[key] = i;
+			console.log(key + "    " + value);
+		};
+		$SP().list(currentListName).get(function(data) {
+			var arrayOfHiddenColumns = [columnGeaendertAm, columnGeaendertVon, columnErfasstAm, columnErstelltVon, columnCopyHistory];
+			var numberOfHiddenColumns = arrayOfHiddenColumns.length;
+			var numberOfRows = data.length;
+			for (var j = 0; j < numberOfRows; j++) {
+				infoText[j] = "";
+				for (var i = 0; i < numberOfHiddenColumns; i++) {
+					var currentColumnName = arrayOfHiddenColumns[i];
+					var currentColumnNumber = nameColumnNumberMap[currentColumnName];
+					var currentColumnDisplayName = nameDisplaynameMap[currentColumnName];
+					var currentEntry = data[j].getAttribute(currentColumnName);
+					if (currentEntry == null) {
+						currentEntry = " ";
+					};
+					if (currentColumnName == columnGeaendertVon || currentColumnName == columnErstelltVon || currentColumnName == columnErfasstAm || currentColumnName == columnGeaendertAm) {
+						var helperArray = currentEntry.split("#");
+						currentEntry = helperArray[1];
+					}
+					infoText[j] += currentColumnDisplayName + ": " + currentEntry + "&lt;br /&gt;";
+				};
+			};
+			var $infoSigns = $("img[src='" + urlInfoSign + "']");
+			$infoSigns.each(function(index, value) {
+				$(this).wrap(bootstrapTooltip + infoText[index] + '"></a>');
+				$(this).closest("a").tooltip();
+			});
+			$SP().lists(function(list) {
+				var availableListsSelect = document.createElement("select");
+				var numberOfAvailableLists = list.length;
+				var typeOfList;
+				var indexToCopy;
+				availableListsSelect.id = "availableListsSelect";
+				//get the select to display only suitable lists for copying
+				if (currentListName.indexOf("LOP") !== -1) {
+					typeOfList = "LOP";
+				} else if (currentListName.indexOf("PUMA") !== -1) {
+					typeOfList = "PUMA"
+				} else {
+					typeOfList = "MeP"
+				};
+				for (var i = 0; i < numberOfAvailableLists; i++) {
+					var currentListChecked = list[i]['Name'];
+					if (currentListChecked.indexOf("LOP") !== -1) {
+						var option = document.createElement("option");
+						option.value = currentListChecked;
+						option.text = currentListChecked;
+						availableListsSelect.appendChild(option);
+					}
+				};
+				$copySigns.each(function(index, value) {
+					$(this).wrap('<a href="#modular" rel="modal:open"></a>');
+					$(this).click(function() {
+						indexToCopy = index;
+					});
+				});
+				document.getElementById("appendSelectHere").appendChild(availableListsSelect);
+				$("#Kopieren").click(function() {
+					console.log("Kopiere Index " + indexToCopy + " in Liste " + $("#availableListsSelect").find(":selected").text());
+					alert(data[indexToCopy].getAttribute("ID"));
+				});
+			});
+		})
+	});
 })
