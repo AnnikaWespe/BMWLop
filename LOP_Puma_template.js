1216d9711ef74c33094490bddd23f5558bec4ead
AnnikaWespe <annika.westphaeling@cgi.com>
2016-11-21 16:21:35 +0100

    Add files via upload

--- LOP_Puma_template.js
+++ LOP_Puma_template.js
@@ -34,13 +34,13 @@ $(document).ready(function() {
             var arrayOfHiddenColumns = [columnGeaendertAm, columnGeaendertVon, columnErfasstAm, columnErstelltVon, columnCopyHistory];
             var numberOfHiddenColumns = arrayOfHiddenColumns.length;
             var numberOfRows = data.length;
+            //while we're at it, we hand over the id to the copy signs
+            $copySigns.each(function(index, value) {
+                $(this).closest("a").data("rowID", data[index].getAttribute("ID"));
+                alert($(this).closest("a").data("rowID"));
+            });
             for (var j = 0; j < numberOfRows; j++) {
                 infoText[j] = "";
-                //while we're at it, we hand over the id to the copy signs
-                $copySigns.each(function(index, value) {
-                    $(this).data("rowID", data[j].getAttribute("ID"));
-                    alert(data[j].getAttribute("ID"));
-                });
                 for (var i = 0; i < numberOfHiddenColumns; i++) {
                     var currentColumnName = arrayOfHiddenColumns[i];
                     var currentColumnNumber = nameColumnNumberMap[currentColumnName];
@@ -60,16 +60,11 @@ $(document).ready(function() {
             $infoSigns.each(function(index, value) {
                 $(this).wrap(bootstrapTooltip + infoText[index] + '"></a>');
                 $(this).closest("a").tooltip();
-            });
-            implementCopyFunction();
+            })
         })
     });
-})
-
-
-var implementCopyFunction = function() {
     $SP().lists(function(list) {
-        var availableListsSelect = "<select id='availableListsSelect' style='margin-bottom: 10px;'>";
+        var availableListsSelect = "<select id='availableListsSelect'>";
         var numberOfAvailableLists = list.length;
         var typeOfList;
         //get the select to display only suitable lists for copying
@@ -81,7 +76,7 @@ var implementCopyFunction = function() {
             typeOfList = "MeP"
         };
         for (var i = 0; i < numberOfAvailableLists; i++) {
-            if (list[i]['Name'].indexOf(typeOfList) !== -1) {
+            if (list[i]['Name'].indexOf("LOP") !== -1) {
                 availableListsSelect += "<option>" + list[i]['Name'] + "</option>";
             }
         };
@@ -89,36 +84,13 @@ var implementCopyFunction = function() {
         $copySigns.each(function(index, value) {
             $(this).wrap('<a href="#modular' + index + '" rel="modal:open"></a>');
             $(".appendModales").append('<div id="modular' + index + '" style="display:none;">' +
-                '<p>In welche Liste möchten Sie die Daten kopieren? ID: ' + $(this).data("rowID") + '</p><p>' + availableListsSelect +
+                '<p>In welche Liste möchten Sie die Daten kopieren?</p><p>' + availableListsSelect +
                 '</p><a href="#" rel="modal:close"><button class = "btn btn-default">Abbrechen</button></a>' +
                 '<button class="btn btn-default" id="Kopiere' + index + '"" style="float: right">Kopieren</button>');
             $("#Kopiere" + index).click(function() {
-                updateListItem();
+                console.log("Kopiere Index " + index + " in Liste " + $("#availableListsSelect option:selected").text());
             });
         });
     });
-}
 
-function updateListItem(itemId) {
-
-    var clientContext = new SP.ClientContext(siteUrl);
-    var oList = clientContext.get_web().get_lists().getByTitle('My List');
-
-    this.oListItem = oList.getItemById(itemId);
-
-    oListItem.set_item('MyField', 'My Updated Field Value');
-
-    oListItem.update();
-
-    clientContext.executeQueryAsync(Function.createDelegate(this, this.onQuerySucceeded), Function.createDelegate(this, this.onQueryFailed));
-}
-
-var onQuerySucceeded = function() {
-
-    alert('Item updated!');
-}
-
-var onQueryFailed = function(sender, args) {
-
-    alert('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
-}
+})
