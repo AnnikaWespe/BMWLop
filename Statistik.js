c724fb8273a5a0552bd2dcc615a711a4605b74a7
AnnikaWespe <annika.wespe@gmail.com>
2016-11-30 17:28:54 +0100

    Add files via upload

--- Statistik.js
+++ Statistik.js
@@ -1,3 +1,5 @@
+var googleChartsColors = ["#3366CC", "#DC3912", "#FF9900", "#109618", "#990099", "#3B3EAC", "#0099C6", "#DD4477", "#66AA00", "#B82E2E", "#316395", "#994499", "#22AA99", "# AAA11", "#6633CC", "#E67300", "#8B0707", "#329262", "#5574A6", "#3B3EAC"];
+
 $(document).ready(function() {
     var modellStat = {};
     var statusStat = {};
@@ -42,10 +44,10 @@ $(document).ready(function() {
             var numberOfRows = data.length;
             console.log(numberOfRows);
             for (var j = 0; j < numberOfRows; j++) {
-                var currentModell = data[j].getAttribute("Modell");
-                var currentStatus = data[j].getAttribute("VSC_x002d_Status");
-                var currentAusleitung = data[j].getAttribute("Ausleitung");
-                var currentVerantwortlicher = data[j].getAttribute("Verantwortlicher");
+                var currentModell = data[j].getAttribute("Modell") || "keine Angabe";
+                var currentStatus = data[j].getAttribute("VSC_x002d_Status") || "keine Angabe";
+                var currentAusleitung = data[j].getAttribute("Ausleitung") || "keine Angabe";
+                var currentVerantwortlicher = data[j].getAttribute("Verantwortlicher") || "keine Angabe";
                 //initialize at 1 or increment according object properties 
                 modellStat[currentModell] = ++modellStat[currentModell] || 1;
                 statusStat[currentStatus] = ++statusStat[currentStatus] || 1;
@@ -94,22 +96,57 @@ var insertData = function(modell, status, ausleitung, verantwortlicher) {
     function drawChart() {
 
         var modellData = google.visualization.arrayToDataTable(modell);
+        var verantwortlicherData = google.visualization.arrayToDataTable(verantwortlicher);
+        var statusData = google.visualization.arrayToDataTable(status);
+        var ausleitungData = google.visualization.arrayToDataTable(ausleitung);
 
-        var modellOptions = {
-            title: 'Modell'
-        };
-        var verantwortlicherOptions = {
-            title: 'Verantwortlicher'
-        };
-        var statusOptions = {
-            title: 'Status'
-        };
-        var ausleitungOptions = {
-            title: 'Ausleitung'
-        };
 
-        var chart = new google.visualization.PieChart(document.getElementById('Modell'));
+        function Options(title) {
+            this.title = title;
+            this.is3D = true;
+            this.width = 300;
+            this.height = 200;
+            this.chartArea = {
+                'width': '100%',
+                'height': '100%'
+            };
+            this.legend = {
+                'position': 'none'
+            };
+            this.pieSliceText = 'label';
+        }
 
-        chart.draw(modellData, modellOptions);
+        var modellOptions = new Options("Modell");
+        var statusOptions = new Options("Status");
+        var ausleitungOptions = new Options("Ausleitung");
+        var verantwortlicherOptions = new Options("Verantwortlicher");
+
+        var modellChart = new google.visualization.PieChart(document.getElementById('Modell'));
+        var verantwortlicherChart = new google.visualization.PieChart(document.getElementById('Verantwortlicher'));
+        var statusChart = new google.visualization.PieChart(document.getElementById('Status'));
+        var ausleitungChart = new google.visualization.PieChart(document.getElementById('Ausleitung'));
+
+        $(".loader").hide();
+        $("#big-table").show();
+        modellChart.draw(modellData, modellOptions);
+        statusChart.draw(statusData, statusOptions);
+        ausleitungChart.draw(ausleitungData, ausleitungOptions);
+        verantwortlicherChart.draw(verantwortlicherData, verantwortlicherOptions);
+
+        createTable(modell, "modellTable");
+        createTable(status, "statusTable");
+        createTable(verantwortlicher, "verantwortlicherTable");
+        createTable(ausleitung, "ausleitungTable");
     }
+}
+
+var createTable = function(array, id) {
+    var tableString = "<tr><th>Farbe</th><th>";
+    var numberOfOptions = array.length;
+    tableString += array[0][0] + "</th><th>" + array[0][1] + "</th></tr>";
+    for (var i = 1; i < numberOfOptions; i++) {
+        tableString += "</tr><td class='showColor'><div class = 'showColor' style= 'background-color:" + googleChartsColors[i - 1] + "''></div></td><td>" + array[i][0] + "</td><td>" + array[i][1] + "</td></tr>";
+        console.log(array[i][1]);
+    }
+    $("#" + id).append(tableString);
 }
